5fbcef273faadce30f53efc24497d0db3dc5b6e9
Luis <luis1198_@hotmail.com>
2015-02-22 16:57:48 -0600

    Cambios a newWeb

--- newWeb.php
+++ newWeb.php
@@ -36,58 +36,60 @@ $teacherId = $user->data()->id;
           </div>
 
           <div class="row"> 
-            <div class="large-4 columns">
-
-              <div>
-                <h6>Nivel 1</h6>
-                <input type="text" name="lbl1"/>
-              </div>
-
-              <div>
-                <h6>Nivel 2</h6>
-                <input type="text" name="lbl1"/>
-              </div>
-
-
+            <div class="weblevels">
+              <ul class="">
+                <li class="level1" onclick="changeLevel(1)"> <h5>Nivel 1</h5> <span id="ql1"> Vacio</span></li>
+                <li class="level2" onclick="changeLevel(2)"> <h5>Nivel 2</h5> <span id="ql2"> Vacio</span></li>
+                <li class="level3" onclick="changeLevel(3)"> <h5>Nivel 3</h5> <span id="ql3"> Vacio</span></li>
+                <li class="addLevel" onclick="addLevel(3)"> <h5> + </h5> <span> Nuevo</span> </li>
+              </ul>
             </div>
 
-            <div class="large-8 columns">
-              <h5>Preguntas disponibles</h5>
-              <div id="filter">
+            <div id="questionFilter" class="questionFilter level1">
 
-               <div class="large-4 columns"> 
-                <label>Tema
+              <div id="filter">
+                <div class="component">
+                  Tema
                   <select id="topic" name="topic"> 
                     <option value="1">1</option> 
                     <option value="2">2</option> 
                   </select> 
-                </label>
-              </div>
+                </div>
 
-              <div class="large-4 columns"> 
-                <label>Dificultad
+                <div class="component">
+                  Dificultad
                   <select id="difficulty" name="difficulty"> 
                     <option value="1">1</option> 
                     <option value="2">2</option> 
                   </select> 
-                </label>
+                </div>
+
+                <a href="#" onclick="filterQuestions()" class="button round tiny right">Get</a>
               </div>
 
-              <a href="#" onclick="filterQuestions()" class="button round tiny right">Get</a>
+              <table class="results"> 
+               <thead> 
+                 <tr> 
+                  <th width="700">Texto Pregunta</th> 
+                  <th width="80">Agregar</th> 
+                </tr> 
+              </thead>
 
-            </div>
-            <div id="container" class="large-8 columns grey1">
-              Hello
+              <tbody> 
 
-            </div>
+              </tbody>
+            </table>
+
           </div>
         </div>
+      </div>
 
-      </form>
+    </form>
 
-    </div>
   </div>
+</div>
 </section>
+
 
 
 <?php include 'includes/templates/footer.php' ?>
@@ -98,10 +100,52 @@ $teacherId = $user->data()->id;
 <script>
   $(document).foundation();
 
+  var currentLevel = 1;
+  var questionsForLevel = [[],[],[]];
+  var usedQuestions = [];
+  var questionFilter = $("#questionFilter");
+
+
+  function changeLevel(level){
+    questionFilter.removeClass("level1");
+    questionFilter.removeClass("level2");
+    questionFilter.removeClass("level3");
+
+    questionFilter.addClass("level"+level);
+    currentLevel = level;
+  }
+
+  function addQuestion(id){
+    //if($.inArray(id, usedQuestions) == -1){
+      questionsForLevel[currentLevel-1].push(id);
+      usedQuestions.push(id);  
+    //}
+    refreshLi();
+  }
+
+  function refreshLi(){
+    var t = "";
+
+    t = questionsForLevel[0].join(",");
+    t = t == "" ? "Vacio" : t;
+    $("#ql1").text(t);    
+    
+    t = questionsForLevel[1].join(",");
+    t = t == "" ? "Vacio" : t;
+    $("#ql2").text(t);
+
+    t = questionsForLevel[2].join(",");
+    t = t == "" ? "Vacio" : t;
+    $("#ql3").text(t);
+
+  }
+
+
   function filterQuestions(){
 
     var topic  = $("#topic").val();
     var difficulty  = $("#difficulty").val();
+    var template =  "<tr id='id'> <td> $text </td><td> <a onclick='addQuestion($id);' class='tiny button secondary'>Agregar</a> </td> </tr>";
 
     $.post( "controls/doAction.php", {  action: "filterQuestions", 
       topic: topic,
@@ -116,6 +160,16 @@ $teacherId = $user->data()->id;
       }else{
         //Llenar el contenedor con las preguntas
         console.log(data);
+        var i;
+        var tbody = $("table tbody");
+        for(i=0; i<data.length; i++){
+          var t = template;
+          console.log(t);
+          t = t.replace("$text", data[i].text);
+          t = t.replace("$id", data[i].id);
+          tbody.append(t);
+
+        }
       }
 
     });
