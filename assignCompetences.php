777e69c66bcdc1b3f8575a0fb90db08823f899ce
Luis <luis1198_@hotmail.com>
2015-04-19 16:38:27 -0500

    evitar competencias duplicadas
    no mostrar competencias que ya fueron asignadas en la pagina de
    assignCompetences
    
    falta hacer la validacion en el bakend esto es solo js y puede ser
    "hackeado"

--- assignCompetences.php
+++ assignCompetences.php
@@ -37,64 +37,84 @@ $groupCompetences = $competence->getCompetencesByGroupOfTeacher($teacherId);
 
         <?php
         foreach ($teacherGroups as $group) {
-          echo "<div class='assignCompetence'><h6 class='panel-title'>Grupo $group->name</h6> <div class='body'><ul>";
-            foreach ($groupCompetences as $competence) {
-              if ($competence->groupId == $group->id){
-                echo "<li>$competence->name <input type='checkbox'> </li>";
-              }
-
+          echo "<div class='assignCompetence' id='g-$group->id'><h6 class='panel-title'>Grupo $group->name</h6> <div class='body'><ul>";
+          foreach ($groupCompetences as $competence) {
+            if ($competence->groupId == $group->id){
+              echo "<li name='$competence->competenceId'>$competence->name </li>";
             }
-            echo "</ul><a href='#' onclick='showAvailableCompetences($group->id)'>Asignar competencia</a></div></div>";
+
           }
-          ?>
+          echo "</ul><a href='#' onclick='showAvailableCompetences($group->id)'>Asignar competencia</a></div></div>";
+        }
+        ?>
 
 
-          <div  class="availableCompetences reveal-modal small" id="acModal" style="display: none" data-reveal>
-            <h3>Competencias disponibles</h3>
-            <ul>
-              <?php
-                foreach ($teacherCompetences as $competence) {
-                  echo "<li>$competence->name <a href='#' onclick='addCompetence($competence->id)'>+</a> </li>";
-                }
-              ?>
-            </ul>
-          </div>
-
+        <div  class="availableCompetences reveal-modal small" id="acModal" style="display: none" data-reveal>
+          <h3>Competencias disponibles</h3>
+          <ul>
+            <?php
+            foreach ($teacherCompetences as $competence) {
+              echo "<li name='$competence->id'>$competence->name <a href='#' onclick='addCompetence($competence->id)'>+</a> </li>";
+            }
+            ?>
+          </ul>
         </div>
+
       </div>
-    </section>
+    </div>
+  </section>
 
+  <?php include 'includes/templates/footer.php' ?>
 
-    <?php include 'includes/templates/footer.php' ?>
+  <script src="js/vendor/jquery.js"></script>
+  <script src="js/foundation.min.js"></script>
+  <script>
+    $(document).foundation();
 
+    var currentGroup = 0;
 
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/foundation.min.js"></script>
-    <script>
-      $(document).foundation();
+    function showAvailableCompetences(groupId) {
+      currentGroup = groupId;
+      var id = "#g-"+groupId;
+      var groupCompetences = $(id).find('li');
 
-      var currentGroup = 0;
+      var availableLis = $(".availableCompetences li");
+      var usedCompetencesIds = [];
 
-      function showAvailableCompetences(groupId) {
-        currentGroup = groupId;
+      //Iteerar cada competencia del grupo actual, guardar su id
+      $(groupCompetences).each(function() {
 
-        $('#acModal').foundation('reveal', 'open');
-        //WAA Filtrar lis de competencias ya usadas
+        var name = $(this).attr('name');
+        usedCompetencesIds.push(name);
+
+      });
+
+      //Iteerar cada competencia disponible y ocultarla si ya existe en el grupo
+      $(availableLis).each(function() {
+        $(this).show();
+        var name = $(this).attr('name');
+        var exists = usedCompetencesIds.indexOf(name); 
+        if(exists != -1){
+          $(this).hide();
+        }        
+      });
+
+    $('#acModal').foundation('reveal', 'open');
+  }
+
+  function addCompetence(competenceId){
+    $.post( "controls/doAction.php", {  action: "addCompetenceToGroup", competenceId: competenceId, groupId: currentGroup})
+    .done(function( data ) {
+      data = JSON.parse(data);
+      if(data.message == 'error'){
+        alert("Error: \n\n" + data.message);
+      }else{
+        window.location.reload();
       }
 
-      function addCompetence(competenceId){
-        $.post( "controls/doAction.php", {  action: "addCompetenceToGroup", competenceId: competenceId, groupId: currentGroup})
-        .done(function( data ) {
-          data = JSON.parse(data);
-          if(data.message == 'error'){
-            alert("Error: \n\n" + data.message);
-          }else{
-            window.location.reload();
-          }
+    });
+  }
 
-        });
-      }
-
-    </script>
-  </body>
-  </html>
+</script>
+</body>
+</html>
